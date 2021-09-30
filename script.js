2012422a84313f84e4ed8a893af969f1264c8a7a
Vinicius Silva <61767666+vinicresende@users.noreply.github.com>
2021-09-29 22:23:36 -0300

    Main (Atividade 9)

--- script.js
+++ script.js
@@ -1,63 +1,45 @@
-class Pessoa {
-    constructor(idade, sexo, opiniao) {
-        this.idade = idade;
-        this.sexo = sexo;
-        this.opiniao = opiniao;
+function exibirResultado(tipo) {
+    const form = document.getElementById('formulario');
+    document.getElementById("Resposta").style.display = "none";
+
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
     }
+
+    const data = new FormData(formulario);
+
+    const numeros = [
+        data.get("fN1"),
+        data.get("fN2"),
+        data.get("fN3")
+    ]
+
+
+    document.getElementById("titulo").innerText = tipo;
+    document.getElementById("resultado").innerText = tipo == 'Ordenação: ' ? ordenacao(numeros) : maior(numeros);
+    document.getElementById("Resposta").style.display = "flex";
+};
+
+function limpar(){
+    document.getElementById("Resposta").style.display = "none";
 }
 
-var pessoas = [];
-
-const encerrar = function () {
-    let media = 0;
-    var idades = [];
-    let pessoaMaisVelha;
-    let pessoaMaisNova;
-    let qtdePessimo = 0;
-    let qtdeOtimoEBom = 0;
-    let qtdeMulheres = 0;
-    let qtdeHomens;
-
-    for (var i = 0; i < pessoas.length; i++) {
-        media += pessoas[i].idade;
-        idades.push(pessoas[i].idade);
-        qtdePessimo += ((pessoas[i].opiniao == 1) ? 1 : 0);
-        qtdeOtimoEBom += ((pessoas[i].opiniao >= 3) ? 1 : 0);
-        qtdeMulheres += ((pessoas[i].sexo.toUpperCase() == 'F') ? 1 : 0);
-    }
-    media /= pessoas.length;
-    pessoaMaisVelha = Math.max.apply(Math, idades);
-    pessoaMaisNova = Math.min.apply(Math, idades);
-    qtdeHomens = pessoas.length - qtdeMulheres;
-    qtdeOtimoEBom = (qtdeOtimoEBom / pessoas.length) * 100;
-
-    alert(
-        `Média de idade: ${media}\n` +
-        `Idade da pessoa mais velha: ${pessoaMaisVelha}\n` +
-        `Idade da pessoa mais nova: ${pessoaMaisNova}\n` +
-        `Pessoas que responderam péssimo: ${qtdePessimo}\n` +
-        `Porcentagem ótimo e bom: ${qtdeOtimoEBom}%\n` +
-        `Número de mulheres: ${qtdeMulheres}\n` +
-        `Número de homens: ${qtdeHomens}\n`
-    );
-
+function exibirOrdenado() {
+    exibirResultado('Ordenação: ');
 }
 
-const adicionarPessoa = function () {
-    pessoas.push(new Pessoa(
-        parseInt(document.getElementById('fidade').value),
-        document.getElementById('fsexo').value,
-        parseInt(document.getElementById('fopiniao').value)
-    ));
-    document.getElementById('textoInicial').innerText = 'Insira os dados da ' + (pessoas.length + 1) + 'ª Pessoa:';
-    //alert(pessoas[0].idade + pessoas[0].sexo + pessoas[0].opiniao);
+function exibirMaximo() {
+    exibirResultado('Maior Numero: ');
 }
 
-document.getElementById("btnEnvUsuario").onclick = function () {
-    adicionarPessoa();
+function ordenacao(numeros) {
+    return (numeros.sort((a, b) => a - b)).join(', ');
 }
 
-document.getElementById("btnEncerrar").onclick = function () {
-    encerrar();
+function maior(numeros) {
+    return numeros.reduce((a, b) => Math.max(a, b));
 }
+
+
 
